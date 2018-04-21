a7d4a528fb4ebedfed2c9d0f7e73698d691e4c27
Matthew Pointer <pointermc@vcu.edu>
2018-04-21 17:53:39 -0400

    App Commit 1

---
+++ src/main/java/com/example/matthew/a508pokemondatabase/QueryPage.java
@@ -0,0 +1,17 @@
+package com.example.matthew.a508pokemondatabase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Spinner;
+
+public class QueryPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_query_page);
+
+       // Spinner querySpinner = (Spinner) findViewById(R.id.querySpinner);
+
+    }
+}
